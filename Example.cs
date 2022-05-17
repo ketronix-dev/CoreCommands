d6adcbe88a8c4889e6ac95bf66ec470fa166ad4e
tfastgame <tfastgame@gmail.com>
2022-05-17 13:19:23 +0300

    Edit Example

--- Example.cs
+++ Example.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Core;
+class Example
+{
+    static void Main()
+    {
+        //no async
+        // string neofetch = CoreCommands.ExecShellCommand("neofetch");
+
+        // with async output
+        // IEnumerable<string> args = new string[]{ "8.8.8.8" };
+        // string ping = CoreCommands.ExecShellCommandAsync("ping", args);
+    }
+}
